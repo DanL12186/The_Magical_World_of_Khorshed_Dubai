b749076ec3aee4a93788407f770d4a4acefad86e
DanL12186 <danlrcn@hotmail.com>
2019-04-04 00:18:23 -0400

    modernize modal code

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,48 +1,43 @@
 $(document).on('turbolinks:load', function() {
+  
   /* Turn.js responsive book */
-  let loadBook = () =>{
+  const loadBook = () => {
     'use strict';
-    var module = {
+    const module = {
       ratio: 2.62,
       init: function (id) {
-        var me = this;
-          console.log(this)
-          // if older browser then don't run javascript
-          if (document.addEventListener) {
-            this.el = document.getElementById(id);
-            this.resize();
-            this.plugins();
-            console.log(this.plugins)
+        this.el = document.getElementById(id);
+        this.resize();
+        this.plugins();
 
-            // on window resize, update the plugin size
-            window.addEventListener('resize', function (e) {
-                var size = me.resize();
-                $(me.el).turn('size', size.width, size.height);
-            });
-          }
+        // on window resize, update the plugin size
+        $(window).on('resize', function() {
+          const size = module.resize();
+          $(module.el).turn('size', size.width, size.height);
+        });
       },
       resize: function () {
         // reset the width and height to the css defaults
         this.el.style.width = '';
         this.el.style.height = '';
 
-        var width = this.el.clientWidth,
+        let width  = this.el.clientWidth,
             height = Math.round(width / this.ratio),
             padded = Math.round(document.body.clientHeight * 0.9);
 
         // if the height is too big for the window, constrain it
         if (height > padded) {
-            height = padded;
-            width = Math.round(height * this.ratio);
+          height = padded;
+          width  = Math.round(height * this.ratio);
         }
 
         // set the width and height matching the aspect ratio
-        this.el.style.width = width + 'px';
-        this.el.style.height = height + 'px';
+        this.el.style.width = `${width}px`
+        this.el.style.height = `${height}px`
 
         return {
-            width: width,
-            height: height
+          width: width,
+          height: height
         };
       },
       plugins: function () {
