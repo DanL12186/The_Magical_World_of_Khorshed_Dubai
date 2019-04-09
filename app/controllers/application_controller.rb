b0ee9ecbbfc49ad4b39bf902ae84263b801e4772
DanL12186 <danlrcn@hotmail.com>
2019-04-09 15:25:25 -0400

    Set featured book on front page by name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def home
-    @book = Book.last
+    @book = Book.find_by(name: "Sandy Bottom")
   end
-
-  # def send_simple_message
-  #   RestClient.post "https://api.mailgun.net/v3/sandboxe2851e4f2a6046c7a8bb9702017d8211.mailgun.org",
-  #   :from => "Dommycakes <sandboxe2851e4f2a6046c7a8bb9702017d8211.mailgun.org>",
-  #   :to => "bar@example.com, daniel.lempesis@gmail.com",
-  #   :subject => "Hello",
-  #   :text => "Testing some Mailgun awesomness!"
-  # end
   
 end
