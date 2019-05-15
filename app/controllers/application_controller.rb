eb11366643d6bd1547870931d5a2b0d2264ea91b
DanL12186 <danlrcn@hotmail.com>
2019-05-14 21:12:13 -0400

    make text dynamic,  based on book info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   def home
     @top_book = Book.find_by(name: "Sir Joe's Quest")
-    @second_book = Book.find_by(name: "Sandy Bottom")
+    @second_book = Book.find_by(name: "Miss Dolphin's Class")
   end
   
 end
