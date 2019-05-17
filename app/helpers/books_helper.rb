f5a9636c833b26edfc70acd66224b680b3c27c6c
DanL12186 <danlrcn@hotmail.com>
2019-05-16 23:29:26 -0400

    remove extraneous helper

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,9 +1,7 @@
 module BooksHelper
+  
   def page_size(format)
     format == 'double' ? '495x380' : '760x990'
   end
 
-  def folder_name(book_name)
-    book_name.downcase.split.join('_')
-  end
 end
