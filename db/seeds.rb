c77bb238b4491f0881a838babe2590e9b8bc6426
DanL12186 <danlrcn@hotmail.com>
2019-05-18 14:27:27 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 Book.create!([
-  {name: "Sir Joe's Quest", category: nil, age_group: nil, format: "single", page_count: 22},
-  {name: "Sandy Bottom", category: nil, age_group: nil, format: "double", page_count: 36}
+  {name: "Sir Joe's Quest", category: nil, age_group: nil, format: "single", page_count: 22, short_summary: "Sir Joe of the Picnic Table goes on a quest to find the source of flame coming from another castle"},
+  {name: "Miss Dolphin's Class", category: nil, age_group: nil, format: "double", page_count: 36, short_summary: "Miss Dolphin teaches a class of young sea creatures."}
 ])
