626ef4539c0760113c78cb65c61b382367253bb3
DanL12186 <danlrcn@hotmail.com>
2019-05-29 14:54:51 -0400

    Further reformatted for better organization and readability

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,27 +1,64 @@
-$(document).on('turbolinks:load', function() {
-  'use strict';
+'use strict';
 
+$(document).on('turbolinks:load', function() {
+  const openedBooks = {};
+
+  //changes all initially-loaded images from webp => jpeg, although also gets rid of fingerprinting
+  const changeStaticWebPToJPG = () => {
+    const staticImages = document.getElementsByClassName('static-img-js')
+
+    for (let i = 0; i < staticImages.length; i++) {
+      const page = staticImages[i]
+      page.src = page.src.replace(/webp/g, 'jpg').replace(/-[^\.jpg]+/, '')
+    }
+  }
+  
   //waits to make sure Modernizr has fired and changed the DOM before determining WebP support
-  const noWebPSupport = new Promise((resolve, _) => {
+  const noWebPSupport = new Promise(resolve => {
     setTimeout(() => {
       const unsupported = !!document.getElementsByClassName('no-webp').length
       resolve(unsupported);
     }, 100);
   });
 
-  const openedBooks = {};
+  //changes all non-CSS, non-lazy images to JPEG if browser lacks WebP support
+  noWebPSupport.then(noWebP => {      
+    if (noWebP) {
+      changeStaticWebPToJPG()
+    }
+  });
 
-  /* Turn.js responsive book */
+  //load book's '.page lazy' images (pages 2 through n-3) on modal click, switching to JPEG if necessary depending on WebP support status 
+  //imperfect solution, as fingerprinting is bypassed completely, even for webp due to lack of rails image_tag
+  const loadImages = bookId => {
+    const lazyPages = document.getElementsByClassName(`page lazy ${bookId}`)
+
+    for (let i = 0; i < lazyPages.length; i++) {
+      const page = lazyPages[i]
+      ,     imageLink = `/assets/${page.getAttribute('image_placeholder')}`
+      
+      noWebPSupport.then(noWebP => {
+        page.src = noWebP ? imageLink.replace(/webp/g, 'jpg') : imageLink
+      })
+    };
+  }
+   
+  /* Load turn.js responsive book */
   function loadBook(bookId) {
     let fitInitialWindow,
         size;
 
     const book = document.getElementById(bookId)
     ,     format = book.getAttribute('data-format')
-    ,     additionalHeight = () => format == 'single' ? size.height * 0.08 : 0
-  
+    ,     additionalHeight = () => format == 'single' ? size.height * 0.08 : 0  
+    
+    const setBookAndModal = () => {
+      size = modal.resize();
+      $(modal.book).turn('size', size.width, size.height + additionalHeight());
+    }
+    
+    //JS object representing the modal, containing the book and its properties + relevant functions
     const modal = {
-      //ratio should eventually be a book object property
       ratio: format == 'single' ? 1.535 : 2.62,
       init: function() {
         this.book = book;
@@ -29,15 +66,13 @@ $(document).on('turbolinks:load', function() {
 
         //properly size on the first click as modal is opened
         if (!fitInitialWindow) {
-          size = modal.resize();
-          $(modal.book).turn('size', size.width, size.height + additionalHeight());
+          setBookAndModal();
           fitInitialWindow = true;
         }
         
         //after first click, on any future window resizing, update the book size
         $(window).on('resize', function() {
-          size = modal.resize();
-          $(modal.book).turn('size', size.width, size.height + additionalHeight());
+          setBookAndModal();
         });
       },
       //gets new size requirements for modal and sets book size
@@ -61,7 +96,7 @@ $(document).on('turbolinks:load', function() {
           height: height
         };
       },
-      //width and height will be set later by init
+      //width and height will be set later by init/resize
       plugins: function () {        
         $(this.book).turn({
             gradients: true,
@@ -79,46 +114,14 @@ $(document).on('turbolinks:load', function() {
     modal.init(bookId);
   };
 
-  //changes webp => jpeg, although also gets rid of fingerprinting
-  function changeStaticWebPToJPG() {
-    const staticImages = document.getElementsByClassName('static-img-js')
-
-    for (let i = 0; i < staticImages.length; i++) {
-      const page = staticImages[i]
-      page.src = page.src.replace(/webp/g, 'jpg').replace(/-[^\.jpg]+/, '')
-    }
-  }
-
-  //this is an imperfect solution, as fingerprinting is bypassed completely, even for webp images
-  function loadImages(bookId) {
-    const lazyPages = document.getElementsByClassName(`page lazy ${bookId}`)
-
-    //load middle pages (2 through n-3)
-    for (let i = 0; i < lazyPages.length; i++) {
-      const page = lazyPages[i]
-      ,     imageLink = `/assets/${page.getAttribute('image_placeholder')}`
-      
-      noWebPSupport.then(noWebP => {
-        page.src = noWebP ? imageLink.replace(/webp/g, 'jpg') : imageLink
-      })
-    };
-  }
-   
-  //change all non-CSS, non-lazy images to JPEG if browser doesn't support WebP
-  noWebPSupport.then(noWebP => {      
-    if (noWebP) {
-      changeStaticWebPToJPG()
-    }
-  });
-
   //when modal is clicked for the first time, load images, load book
   $("#pj-section-pic-1-div, #top-book-pic-1-div").on('click', function() {
     const bookId = this.getAttribute('data-bookname')
-    
-      if (!openedBooks[bookId]) {
-        loadImages(bookId);
-        loadBook(bookId);
-        openedBooks[bookId] = true
-      }
+  
+    if (!openedBooks[bookId]) {
+      loadImages(bookId);
+      loadBook(bookId);
+      openedBooks[bookId] = true
+    }
   })
-})
+});
