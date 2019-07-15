ff91e0c57043adb29fe00429ec90e5840d1120dc
DanL12186 <daniel.lempesis@gmail.com>
2019-07-15 10:57:31 -0400

    Add pixel_width/height data to seeds for proper operation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Book.create!([
-  {name: "Sir Joe's Quest", category: nil, age_group: nil, format: "single", page_count: 22, short_summary: "Sir Joe of the Picnic Table goes on a quest to find the source of flame coming from another castle"},
-  {name: "Miss Dolphin's Class", category: nil, age_group: nil, format: "double", page_count: 36, short_summary: "Miss Dolphin teaches a class of young sea creatures."},
-  {name: "Will You Be My Friend?", category: nil, age_group: "preschool", format: "double", page_count: 18, short_summary: "A young dragon makes a new friend"}
+  {name: "Sir Joe's Quest", category: nil, age_group: nil, format: "single", page_count: 22, short_summary: "Sir Joe of the Picnic Table goes on a quest to find the source of flame coming from another castle", pixel_height: 1050, pixel_width: 752},
+  {name: "Miss Dolphin's Class", category: nil, age_group: nil, format: "double", page_count: 36, short_summary: "Miss Dolphin teaches a class of young sea creatures.", pixel_height: 937, pixel_width: 1237},
+  {name: "Will You Be My Friend?", category: nil, age_group: "preschool", format: "double", page_count: 18, short_summary: "A young dragon makes a new friend", pixel_height: 950, pixel_width: 1200}
 ])
