939b0153280571c1fdbf38606acb65dda7274346
DanL12186 <daniel.lempesis@gmail.com>
2019-07-19 15:35:11 -0400

    Remove unused paths

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module TheMagicalWorldOfKhorshedDubai
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.paths << Rails.root.join('node_modules/startbootstrap-grayscale/js')
-    config.assets.paths << Rails.root.join('node_modules/startbootstrap-grayscale/scss')
     #enable text compression
     config.middleware.insert_after(::Rack::Runtime, Rack::Deflater)
     
