8219b4befde36c56b2aa8610d7a84ab0c5f9d1fe
DanL12186 <daniel.lempesis@gmail.com>
2019-07-30 17:37:14 -0400

    Add @message to home controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
     @top_book = Book.find_by(name: "Sir Joe's Quest")
     @second_book = Book.find_by(name: "Miss Dolphin's Class")
     @third_book = Book.find_by(name: "Will You Be My Friend?")
+
+    @message = Message.new
   end
   
 end
