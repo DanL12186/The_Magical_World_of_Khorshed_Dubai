158534689122acf27c65b0b7263618a9f83a831e
DanL12186 <daniel.lempesis@gmail.com>
2019-07-30 17:36:57 -0400

    Remove needless action, no longer redirect to root as it's already on root

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,12 @@
 class MessagesController < ApplicationController
-  
-  def new
-    @message = Message.new
-  end
 
   def create
     @message = Message.new(message_params)
     if @message.valid?
       MessageMailer.contact(@message).deliver_now
       flash[:notice] = "We have received your message and will be in touch soon!"
-      redirect_to root_path
     else
       flash[:notice] = "There was an error sending your message. Please try again."
-      redirect_to messages_path
     end
   end
 
