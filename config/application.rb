e31298bdf9ea5a46c4c000e62eedd6a3eaef74b1
DanL12186 <daniel.lempesis@gmail.com>
2019-08-22 13:46:53 -0400

    Change config.load_defaults to 6.0, since rails is now running 6.0

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module TheMagicalWorldOfKhorshedDubai
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
     #enable text compression
     config.middleware.insert_after(::Rack::Runtime, Rack::Deflater)
     
