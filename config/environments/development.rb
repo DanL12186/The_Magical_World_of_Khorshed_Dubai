cbaf0b05ae20f062effdba2a9e05e2e4a3f6d014
DanL12186 <DanL12186@users.noreply.github.com>
2020-05-27 16:10:30 -0500

    remove action_cable and active_storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
-
   # Don't care if the mailer can't send.
   config.action_mailer.delivery_method = :mailgun
   
@@ -37,7 +34,6 @@ Rails.application.configure do
    api_key: 'ENV["MAILGUN_API_KEY"]',
    domain: "sandboxe2851e4f2a6046c7a8bb9702017d8211.mailgun.org",
   }
-
 
   config.action_mailer.raise_delivery_errors = false
 
