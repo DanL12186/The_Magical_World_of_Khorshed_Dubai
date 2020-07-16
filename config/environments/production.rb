71c499f552c84c51d7571968ebfe0c3cf11e2766
DanL12186 <DanL12186@users.noreply.github.com>
2020-07-16 00:14:24 -0500

    test to see if heroku will run without config.assets.compile set to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,6 @@ Rails.application.configure do
    domain: "https://thawing-sea-93219.herokuapp.com",
   }
 
-
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
@@ -35,7 +34,7 @@ Rails.application.configure do
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
