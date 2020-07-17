eb4161ad56cc774b18e1d8a5ee27f289867e8ce4
DanL12186 <DanL12186@users.noreply.github.com>
2020-07-16 20:25:22 -0500

    one query instead of three

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def home
-    @top_book = Book.find_by(name: "Sir Joe's Quest")
-    @second_book = Book.find_by(name: "Miss Dolphin's Class")
-    @third_book = Book.find_by(name: "Will You Be My Friend?")
-
+    @top_book, @second_book, @third_book = Book.all
     @message = Message.new
   end
   
